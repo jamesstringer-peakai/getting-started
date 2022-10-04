db3d5f5b88b90af553ff175bd4e764eb8e1e49df
James Stringer <james.stringer@peak.ai>
2022-10-04 16:55:38 +0100

    remove dodgy quotes

--- workspace/Pulling data from a database (R).R
+++ workspace/Pulling data from a database (R).R
@@ -1,6 +1,6 @@
 ## Install the connector you want to use 
-install.packages(‘DBI’)
-install.packages(‘odbc’)
+install.packages("DBI")
+install.packages("odbc")
 install.packages("tidyverse")
 
 # Load the packages you need
@@ -9,7 +9,7 @@ library(odbc)
 library(tidyverse)
 
 # Create a connection string using the `odbc` file within a Workspace
-conn <- dbConnect(odbc(), paste0(Sys.getenv('TENANT') , '-' , Sys.getenv('STAGE')))
+conn <- dbConnect(odbc(), paste0(Sys.getenv("TENANT") , '-' , Sys.getenv("STAGE")))
 
 # Pull the data from Snowflake into a Workspace
-df <- dbGetQuery(conn, “select * from STAGE.HOUSE_PRICES”)
+df <- dbGetQuery(conn, "select * from STAGE.HOUSE_PRICES")
